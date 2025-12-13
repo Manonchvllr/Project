4feaff558c5e774aaa6b8444d39b2a6c247eaefc
Sara CHIKHI <sarachikhi@macbook-air-1.home>
2025-12-13 14:17:46 +0100

    mise en forme cartes

--- src/package_project/cartes.py
+++ src/package_project/cartes.py
@@ -1,3 +1,4 @@
+from package_project import indicateurs_climat
 import geopandas as gpd
 from cartiflette import carti_download
 import matplotlib.pyplot as plt
@@ -6,7 +7,9 @@ from matplotlib.colors import TwoSlopeNorm
 def remove_leading_zeros(num):
     return num.lstrip('0')
 
-def carte(data, indicateur, titre_axe, titre_carte):
+
+
+def donnee_carte(data):
 # Charger les départements français depuis une source publique
     france = carti_download(
         values = ["France"],
@@ -21,40 +24,55 @@ def carte(data, indicateur, titre_axe, titre_carte):
     france["INSEE_DEP"] = france.INSEE_DEP.astype(str)
     france["INSEE_DEP"] = france["INSEE_DEP"].str.lstrip('0').fillna(value='0')
     carte = france.merge(data, left_on="INSEE_DEP", right_on="DEP", how="inner")
-    # Afficher la carte avec les noms des départements
-    # Normalisation centrée sur zéro
-    if carte[indicateur].min() <0:
-        norm = TwoSlopeNorm(vmin=carte[indicateur].min(),
-                            vmax=carte[indicateur].max(),
-                            vcenter=0)
+    return(carte)
+
+
+
+def mise_en_forme_carte(data, annees, mois, indicateur, titre_carte, plotting, evolution):
+
+    if evolution == False:
+        titre_axe="Nombre de jours"
+        carte=donnee_carte(indicateurs_climat.nbj_par_an(data, annees, mois, indicateur))
+
+        indic=indicateur
+    elif evolution == True:
+        titre_axe="Taux de variation"
+        if all(item in [6,7,8,9] for item in mois):
+            saison = ["été"]         #  rouge
+        elif all(item in [1,2,3,12] for item in mois):
+            saison= ['hiver']
+        indic= "evol_2015"
+        carte=donnee_carte(indicateurs_climat.nbj_evol_2015(data, saison, indicateur))
+        cols="bwr"          # bleu blanc rouge
+
+    if carte[indic].min() < 0:
+        center = 0
+        cols="bwr"
     else:
-        norm = TwoSlopeNorm(vmin=carte[indicateur].min(),
-                            vmax=carte[indicateur].max(),
-                            vcenter = carte[indicateur].median())
+        center = (carte[indic].max() - carte[indic].min())/2
+        if all(item in [6,7,8,9] for item in mois):
+            cols='OrRd'         #  rouge
+        elif all(item in [1,2,3,12] for item in mois):
+            cols='Blues'
 
-    # Affichage
-    fig, ax = plt.subplots(figsize=(10, 10))
+    norme = TwoSlopeNorm(vmin=carte[indic].min(),
+            vmax=carte[indic].max(),
+            vcenter=center) 
+    
     carte.plot(
-        column=indicateur,
-        cmap="bwr",          # bleu blanc rouge
-        norm=norm,           # <-- centrage sur 0
-        linewidth=0.8,
-        ax=ax,
-        edgecolor="0.8",
-        legend=True,
-        legend_kwds={
-            "shrink": 0.5,
-            "label": titre_axe,
-            "orientation": "vertical"
-        }
-    )
-    # Ajouter les noms des départements
-    for idx, row in carte.iterrows():
-        centroid = row['geometry'].centroid
-        ax.text(centroid.x, centroid.y, row['LIBELLE_DEPARTEMENT'],
-                fontsize=8, ha='center', va='center')
-
-    ax.set_title(titre_carte, fontsize=15)
-    ax.axis("off")
-    plt.tight_layout()
-    plt.show()
+         column=indic,
+         cmap=cols,
+         norm=norme,           # <-- centrage sur 0
+         linewidth=0.8,
+         ax=plotting,
+         edgecolor="0.8",
+         legend=True,
+         legend_kwds={
+             "shrink": 0.3,
+             "label": titre_axe,
+             "orientation": "vertical"
+         }
+     )
+    
+    plotting.set_title(titre_carte, fontsize=15)
+    plotting.axis("off")
